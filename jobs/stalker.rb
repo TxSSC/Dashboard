a39b3f485299bf88391c2ab5ee44cfbfa78a31f3
Matt McFarland <matmcfarland@gmail.com>
2013-04-05 15:33:51 -0500

    update stalker job

--- jobs/stalker.rb
+++ jobs/stalker.rb
@@ -1,16 +1,18 @@
 require 'net/http'
 require 'json'
 
-SCHEDULER.every '5m', :first_in => 0 do |job|
+SCHEDULER.every '2m', :first_in => 0 do |job|
   http = Net::HTTP.new('stalker.texasschoolsafetycenter.com')
   response = http.request(Net::HTTP::Get.new("/users"))
   users = JSON.parse(response.body)
 
   if users
     users.map! do |user|
-      { avatar: user['avatar'], location: user['location'], returning: user['returning'] }
+      { name: user['name'].capitalize,
+        status: user['location'].match(/^\s*(?:strahan|strahan\s+house)\s*$/i) != nil
+      }
     end
 
-    send_event('stalker', users: users)
+    send_event('stalker', data: users)
   end
 end
