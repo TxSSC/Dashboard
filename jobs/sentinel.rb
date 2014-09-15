7c498b519ad51c3d33bd9b9bbb53bb8d4b912520
Matt McFarland <matmcfarland@gmail.com>
2014-09-15 14:26:04 -0500

    update sentinel server path

--- jobs/sentinel.rb
+++ jobs/sentinel.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'net/http'
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  http = Net::HTTP.new('sentinel.texasschoolsafetycenter.com')
+  http = Net::HTTP.new('sentinel.txssc.com')
   response = http.request(Net::HTTP::Get.new("/"))
   servers = JSON.parse(response.body)
 
